70e487f6a2243573daae4bb4aa5d10a1c9bb3b21
Hoda Salah <37372736+hodasalah@users.noreply.github.com>
2022-05-11 13:44:43 +0300

    change color

--- src/flow/Flow.tsx
+++ src/flow/Flow.tsx
@@ -16,7 +16,7 @@ export default function Flow() {
 	const [edges, setEdges] = useState<EdgeObject[]>(treeEdges);
 	const [collapse, setCollapse] = useState(true);
 
-	const onNodeClicked = (nodeId) => {
+	const onNodeClicked = (nodeId, node) => {
 		if (nodeId === "1") {
 			if (collapse) {
 				setEdges([]);
@@ -30,33 +30,40 @@ export default function Flow() {
 		let selectedNode = treeNodes.filter(
 			(n) => n.id === nodeId && n.data?.nodes
 		);
+		console.log(selectedNode);
+		//parent? [{node}]:[]
 		//get selected edges
 		let selectedEdges = treeEdges.filter((e) => {
 			return e.data?.parents?.includes(nodeId);
 		});
+		console.log(selectedEdges);
 		//check if selectedNode array no empty
 		if (selectedNode.length) {
 			// get nodes by ids
 			let getNodes = selectedNode[0]?.data?.nodes
 				.map((n) => treeNodes.filter((node) => node.id === n.id))
 				.reduce((p, n) => [...p, ...n]);
+			console.log(getNodes);
 			setCollapse(!collapse);
+			let nItems = [...nodes].filter(function (el) {
+				return !getNodes.map((n) => n.id).includes(el.id);
+			});
+
+			let eItems = [...edges].filter(function (el) {
+				return !selectedEdges.map((e) => e.id).includes(el.id);
+			});
+
 			if (collapse) {
 				//return all [nodes || edges] without children nodes for selected item
-				let nItems = [...nodes].filter(function (el) {
-					return !getNodes.map((n) => n.id).includes(el.id);
-				});
 
-				let eItems = [...edges].filter(function (el) {
-					return !selectedEdges.map((e) => e.id).includes(el.id);
-				});
+				console.log(eItems);
 				setNodes(nItems);
 				setEdges(eItems);
 			}
 			if (!collapse) {
 				nodes.forEach((el) => {
 					if (!getNodes.includes(el)) {
-						setNodes([...nodes, ...getNodes]);
+						setNodes([...nItems, ...getNodes]);
 					} else {
 						setNodes([...nodes]);
 					}
@@ -64,12 +71,13 @@ export default function Flow() {
 				edges.forEach((el) => {
 					// to be sure that we add elem one time
 					if (!selectedEdges.includes(el)) {
-						setEdges([...edges, ...selectedEdges]);
+						setEdges([...eItems, ...selectedEdges]);
 					} else {
 						setEdges([...edges]);
 					}
 				});
 				setCollapse(!collapse); // to not repeat elements
+				console.log(node);
 			}
 		}
 	};
@@ -91,13 +99,25 @@ export default function Flow() {
 				node={(node: NodeProps) => (
 					<Node
 						{...node}
-						onClick={() => onNodeClicked(node.id)}
+						onClick={() => onNodeClicked(node.id, node)}
 						style={{
-							stroke: "#1a192b",
-							fill: "#1a192b",
+							stroke: node.properties.data?.nodes?.length
+								? "red"
+								: "#1a192b",
+							fill: node.properties.data?.nodes?.length
+								? "red"
+								: "#1a192b",
 							strokeWidth: 1,
 						}}
-						label={<Label style={{ fill: "black" }} />}
+						label={
+							<Label
+								style={{
+									fill: node.properties.data?.nodes?.length
+										? "red"
+										: "black",
+								}}
+							/>
+						}
 						port={
 							<Port
 								style={{
